c894685c0364783ef3a829a9ec8cf6e14436cb3c
Андрейс Жестянниковс <azhestyannikovs@edu.hse.ru>
2020-06-03 18:06:21 +0300

    version4

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // RedLight
             // 
-            this.RedLight.Location = new System.Drawing.Point(159, 12);
+            this.RedLight.Location = new System.Drawing.Point(118, 12);
             this.RedLight.Name = "RedLight";
             this.RedLight.Size = new System.Drawing.Size(150, 150);
             this.RedLight.TabIndex = 0;
@@ -46,7 +46,7 @@
             // 
             // YellowLight
             // 
-            this.YellowLight.Location = new System.Drawing.Point(159, 181);
+            this.YellowLight.Location = new System.Drawing.Point(118, 180);
             this.YellowLight.Name = "YellowLight";
             this.YellowLight.Size = new System.Drawing.Size(150, 150);
             this.YellowLight.TabIndex = 1;
@@ -54,7 +54,7 @@
             // 
             // GreenLight
             // 
-            this.GreenLight.Location = new System.Drawing.Point(159, 354);
+            this.GreenLight.Location = new System.Drawing.Point(118, 350);
             this.GreenLight.Name = "GreenLight";
             this.GreenLight.Size = new System.Drawing.Size(150, 150);
             this.GreenLight.TabIndex = 2;
@@ -64,7 +64,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(572, 525);
+            this.ClientSize = new System.Drawing.Size(446, 525);
             this.Controls.Add(this.GreenLight);
             this.Controls.Add(this.YellowLight);
             this.Controls.Add(this.RedLight);
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,61 @@ namespace TrafficLights
 {
     public partial class Form1 : Form
     {
+
+        private Timer timerSwitch;
+
+
         public Form1()
         {
             InitializeComponent();
+            InitalizeTrafficLights();
+            InitializeTimerSwitch();
+        }
+        private void InitializeTimerSwitch()
+        {
+           timerSwitch = new Timer();
+           timerSwitch.Interval = 3000;
+           timerSwitch.Tick += new EventHandler(TimerSwitch_Tick);
+           timerSwitch.Start();
+
+        }
+
+        private void TimerSwitch_Tick(object sender, EventArgs e)
+        {
+            if(RedLight.BackColor == Color.Gray)
+            {
+                RedLight.BackColor = Color.Red;
+            
+            }
+            else
+            {
+                RedLight.BackColor = Color.Gray;
+                
+            }
+            if(YellowLight.BackColor == Color.Gray)
+            {
+                YellowLight.BackColor = Color.Yellow;
+            }
+            else
+            {
+                YellowLight.BackColor = Color.Gray;
+            }
+            if (GreenLight.BackColor == Color.Gray)
+            {
+                GreenLight.BackColor = Color.Green;
+            }
+            else
+            {
+                GreenLight.BackColor = Color.Green;
+            }
+        }
+
+
+        private void InitalizeTrafficLights()
+        {
+            RedLight.BackColor = Color.Gray;
+            YellowLight.BackColor = Color.Gray;
+            GreenLight.BackColor = Color.Gray;
         }
     }
 }
