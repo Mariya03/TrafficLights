82e3f575909f97a388aec6cedc0f8f39cdcf4694
Андрейс Жестянниковс <azhestyannikovs@edu.hse.ru>
2020-06-06 17:30:12 +0300

    version5

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,8 @@ namespace TrafficLights
     {
 
         private Timer timerSwitch;
+        private int timeCounter = 0;
+
 
 
         public Form1()
@@ -25,7 +27,7 @@ namespace TrafficLights
         private void InitializeTimerSwitch()
         {
            timerSwitch = new Timer();
-           timerSwitch.Interval = 3000;
+           timerSwitch.Interval = 1000;
            timerSwitch.Tick += new EventHandler(TimerSwitch_Tick);
            timerSwitch.Start();
 
@@ -33,32 +35,34 @@ namespace TrafficLights
 
         private void TimerSwitch_Tick(object sender, EventArgs e)
         {
-            if(RedLight.BackColor == Color.Gray)
+            if(timeCounter == 0)
             {
                 RedLight.BackColor = Color.Red;
-            
             }
-            else
+            else if(timeCounter == 3)
             {
                 RedLight.BackColor = Color.Gray;
+                YellowLight.BackColor = Color.Yellow; 
                 
             }
-            if(YellowLight.BackColor == Color.Gray)
-            {
-                YellowLight.BackColor = Color.Yellow;
-            }
-            else
+            else if(timeCounter == 6)
             {
                 YellowLight.BackColor = Color.Gray;
-            }
-            if (GreenLight.BackColor == Color.Gray)
-            {
                 GreenLight.BackColor = Color.Green;
+               
             }
-            else
+            else if (timeCounter == 9)
             {
-                GreenLight.BackColor = Color.Green;
+                GreenLight.BackColor = Color.Gray;
+                YellowLight.BackColor = Color.Yellow;
             }
+            else if(timeCounter == 12)
+            {
+                YellowLight.BackColor = Color.Gray;
+                RedLight.BackColor = Color.Red;
+                timeCounter = -1;
+            }
+            timeCounter++;
         }
 
 
